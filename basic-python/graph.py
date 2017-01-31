f7927479a920091a66e187c79a5f9b16ba2a38cd
DONGSHIH TENG <tou9702@gmail.com>
2017-01-31 18:56:06 +0900

    Add cos graph

--- basic-python/graph.py
+++ basic-python/graph.py
@@ -3,7 +3,14 @@ import matplotlib.pyplot as plt
 
 # Set data
 x = np.arange(0, 10, 0.1) # 0 to 6 with 0.1 interval
-y = np.sin(x)
 
-plt.plot(x, y)
+y1 = np.sin(x)
+y2 = np.cos(x)
+
+plt.plot(x, y1, label="sin")
+plt.plot(x, y2, linestyle = "--", label="cos")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("sin and cos")
+plt.legend()
 plt.show()
