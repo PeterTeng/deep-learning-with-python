fe681b2d08d46602f795e8700d68db79778cc732
DONGSHIH TENG <tou9702@gmail.com>
2017-02-01 01:53:01 +0900

    Add AND with bias

--- perceptron/and.py
+++ perceptron/and.py
@@ -1,4 +1,4 @@
-import numpy as ap
+import numpy as np
 
 # Easy way
 def EasyAND(x1, x2):
@@ -8,3 +8,18 @@ def EasyAND(x1, x2):
         return 0
     elif tmp > theta:
         return 1
+
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7 #=> theta = 0
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+print(AND(0, 0))
+print(AND(1, 0))
+print(AND(0, 1))
+print(AND(1, 1))
