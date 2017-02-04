69e3452601f08418dc95b7a48980532e392faac3
DONGSHIH TENG <tou9702@gmail.com>
2017-02-04 23:33:34 +0900

    Add step_function with numpy

--- neural-network/step_function.py
+++ neural-network/step_function.py
@@ -1,6 +1,18 @@
+import numpy as np
+import matplotlib.pylab as plt
+
 # Easy way
 def easy_step_function(x):
     if x > 0:
         return 1;
     else:
         return 0;
+
+def ste_function(x):
+    return np.array(x > 0, dtype=np.int)
+
+x = np.arange(-5.0, 5.0, 0.05)
+y = ste_function(x)
+plt.plot(x, y)
+plt.ylim(-0.2, 1.2)
+plt.show()
