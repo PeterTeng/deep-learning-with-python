5bb36a7fc93d15f9223c26298fb42fd0c085ede5
DONGSHIH TENG <tou9702@gmail.com>
2017-02-05 22:29:24 +0900

    Fix typo

--- neural-network/step_function.py
+++ neural-network/step_function.py
@@ -8,11 +8,11 @@ def easy_step_function(x):
     else:
         return 0;
 
-def ste_function(x):
+def step_function(x):
     return np.array(x > 0, dtype=np.int)
 
 x = np.arange(-5.0, 5.0, 0.05)
-y = ste_function(x)
+y = step_function(x)
 plt.plot(x, y)
 plt.ylim(-0.2, 1.2)
 plt.show()
