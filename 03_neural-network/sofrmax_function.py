a002e06229186b01c4d434370e286df0c6b8eb40
DONGSHIH TENG <tou9702@gmail.com>
2017-06-25 23:12:33 +0900

    Add sum of softmax function

--- 03_neural-network/sofrmax_function.py
+++ 03_neural-network/sofrmax_function.py
@@ -19,3 +19,11 @@ def softmax(a):
 
 A = np.array([0.3, 2.9, 4.0])
 print(softmax(A))
+
+print("SUM of softmax(A) =")
+print(np.sum(softmax(A)))
+
+# Output
+# [ 0.01821127  0.24519181  0.73659691]
+# SUM of softmax(A) =
+# 1.0
