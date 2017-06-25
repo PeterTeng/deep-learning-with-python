0cd062382e927f6e81591292fcc95d6eb5772e06
DONGSHIH TENG <tou9702@gmail.com>
2017-06-25 23:10:15 +0900

    Fix

--- 03_neural-network/sofrmax_function.py
+++ 03_neural-network/sofrmax_function.py
@@ -1,7 +1,17 @@
 import numpy as np
 
+# Simple way
+# def softmax(a):
+#     exp_a = np.exp(a)
+#     sum_exp_a = np.sum(exp_a)
+#     y = exp_a / sum_exp_a
+#
+#     return y
+
+# Prevent Overflow
 def softmax(a):
-    exp_a = np.exp(a)
+    c = np.max(a)
+    exp_a = np.exp(a - c)
     sum_exp_a = np.sum(exp_a)
     y = exp_a / sum_exp_a
 
