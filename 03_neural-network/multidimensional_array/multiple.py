d2eb52e05a410907f2ca3bb11bd9ad8cc8980785
DONGSHIH TENG <tou9702@gmail.com>
2017-06-25 23:03:11 +0900

    Add example with identity_func

--- 03_neural-network/multidimensional_array/multiple.py
+++ 03_neural-network/multidimensional_array/multiple.py
@@ -3,6 +3,9 @@ import numpy as np
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
+def identity_func(x):
+    return x
+
 X = np.array([1.0, 0.5])
 print(X)
 
@@ -18,3 +21,18 @@ print(A1)
 Z1 = sigmoid(A1)
 print("Z1 = ")
 print(Z1)
+
+W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+B2 = np.array([0.1, 0.2])
+
+A2 = np.dot(Z1, W2) + B2
+Z2 = sigmoid(A2)
+
+W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+B3 = np.array([0.1, 0.2])
+
+A3 = np.dot(Z2, W3) + B3
+
+Y = identity_func(A3)
+
+print(Y)
