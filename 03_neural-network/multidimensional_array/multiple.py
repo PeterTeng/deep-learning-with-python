c1cf7e4ec46ddc4c57f9ac1b6dc8147fd2b0b141
DONGSHIH TENG <tou9702@gmail.com>
2017-06-25 13:55:40 +0900

    Use sigmoid

--- 03_neural-network/multidimensional_array/multiple.py
+++ 03_neural-network/multidimensional_array/multiple.py
@@ -1,11 +1,7 @@
 import numpy as np
 
-# A = np.array([[1, 2], [3, 4]])
-# B = np.array([[5, 6], [7, 8]])
-
-# multiple = np.dot(A, B)
-
-# print(multiple)
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 X = np.array([1.0, 0.5])
 print(X)
@@ -15,8 +11,10 @@ B1 = np.array([0.1, 0.2, 0.3]) # Bias
 print(W1)
 print(B1)
 
-print("XW + B = ")
+print("A1 = XW1 + B1 = ")
+A1 = np.dot(X, W1) + B1
+print(A1)
 
-Y = np.dot(X, W1) + B1
-
-print(Y)
+Z1 = sigmoid(A1)
+print("Z1 = ")
+print(Z1)
