f37555d2b2f21438695c7d0f47f8354ad984bf2c
DONGSHIH TENG <tou9702@gmail.com>
2017-06-25 12:19:10 +0900

    Array usage on neural network

--- 03_neural-network/multidimensional_array/multiple.py
+++ 03_neural-network/multidimensional_array/multiple.py
@@ -1,9 +1,18 @@
 import numpy as np
 
-A = np.array([[1, 2], [3, 4]])
-B = np.array([[5, 6], [7, 8]])
+# A = np.array([[1, 2], [3, 4]])
+# B = np.array([[5, 6], [7, 8]])
 
-multiple = np.dot(A, B)
+# multiple = np.dot(A, B)
 
-print(multiple)
+# print(multiple)
 
+X = np.array([1, 2])
+print(X)
+
+W = np.array([[1, 3, 5], [2, 4, 6]])
+print(W)
+
+Y = np.dot(X, W)
+
+print(Y)
